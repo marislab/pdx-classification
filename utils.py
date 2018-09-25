b757b3c9cb01e50e3a58d11ad7fa544924e43a66
gwaygenomics <gregory.way@gmail.com>
2018-09-25 09:56:01 -0400

    add evaluation helper functions

--- utils.py
+++ utils.py
@@ -58,3 +58,129 @@ def shuffle_columns(gene):
     """
     import numpy as np
     return np.random.permutation(gene.tolist())
+
+
+def perform_ttest(df, gene):
+    """
+    Perform an independent ttest for the status predictions for each gene class
+
+    Arguments:
+    df - the dataframe of scores
+    gene - the name of the gene to subset the scores dataframe
+    """
+    from scipy.stats import ttest_ind
+
+    lower_gene = gene.lower()
+
+    if lower_gene not in ['ras', 'nf1', 'tp53']:
+        raise ValueError('Enter either "ras", "nf1", or "tp53" as the gene')
+
+    status = '{}_status'.format(lower_gene)
+    score = '{}_score'.format(lower_gene)
+
+    one_status = '{} == 1'.format(status)
+    zero_status = '{} == 0'.format(status)
+
+    t_results = ttest_ind(
+        a=df.query(one_status).loc[:, score],
+        b=df.query(zero_status).loc[:, score],
+        equal_var=False
+        )
+
+    return t_results
+
+
+def get_mutant_boxplot(df, gene, t_test_results=None, histology=False,
+                       hist_color_dict=None):
+    """
+    Show boxplot of classifier scores stratified by binary gene mutation status
+
+    Arguments:
+    df - the dataframe of scores
+    gene - the name of the gene to input
+    t_test_results - the output of `perform_ttest` for the specific gene
+    histology - boolean if the boxplot should stratify by histology
+    hist_color_dict - a dictionary storing prespecified hex colors by histology
+    """
+    import os
+    from decimal import Decimal
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Set plotting constants
+    x1, x2 = 0, 1
+    y1, h = 0.98, 0.03
+
+    lower_gene = gene.lower()
+
+    if lower_gene not in ['ras', 'nf1', 'tp53']:
+        raise ValueError('Enter either "ras", "nf1", or "tp53" as the gene')
+
+    x = '{}_status'.format(lower_gene)
+    y = '{}_score'.format(lower_gene)
+    xticklabels = ['{} Wild-Type'.format(gene), '{} Mutant'.format(gene)]
+
+    if histology:
+        output_file = os.path.join('figures',
+                                   'histology_{}_predictions.pdf'.format(gene))
+        plt.rcParams['figure.figsize'] = (8, 4)
+        ax = sns.boxplot(x=x,
+                         y=y,
+                         data=df,
+                         hue='Histology_Full',
+                         color='white',
+                         fliersize=0)
+
+        ax = sns.stripplot(x=x,
+                           y=y,
+                           data=df,
+                           hue='Histology_Full',
+                           palette=hist_color_dict,
+                           dodge=True,
+                           edgecolor='black',
+                           jitter=0.25,
+                           size=4,
+                           alpha=0.65)
+
+        handles, labels = ax.get_legend_handles_labels()
+
+        lgd = plt.legend(handles[19:41], labels[19:41],
+                         bbox_to_anchor=(1.03, 1),
+                         loc=2,
+                         borderaxespad=0.,
+                         fontsize=8)
+        lgd.set_title("Histology")
+
+    else:
+        output_file = os.path.join('figures',
+                                   '{}_predictions.pdf'.format(gene))
+        plt.rcParams['figure.figsize'] = (3.5, 4)
+        ax = sns.boxplot(x=x,
+                         y=y,
+                         data=df,
+                         palette='Greys',
+                         fliersize=0)
+
+        ax = sns.stripplot(x=x,
+                           y=y,
+                           data=df,
+                           dodge=True,
+                           edgecolor='black',
+                           jitter=0.25,
+                           size=4,
+                           alpha=0.65)
+
+    ax.set_ylabel('Classifier Score', fontsize=12)
+    ax.set_xlabel('Samples', fontsize=12)
+    ax.set_xticklabels(xticklabels)
+
+    # Add Ras T-Test Results
+    if not histology:
+        plt.plot([x1, x1, x2, x2], [y1, y1+h, y1+h, y1], lw=1.2, c='black')
+        plt.text(.5, y1+h, "{:.2E}".format(Decimal(t_test_results.pvalue)),
+                 ha='center', va='bottom', color="black")
+
+    plt.axhline(linewidth=2, y=0.5, color='black', linestyle='dashed')
+    plt.tight_layout()
+    plt.savefig(output_file)
+    plt.show()
